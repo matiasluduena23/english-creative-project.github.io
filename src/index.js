6ea1f85a31717fe841fe2c07a1f3b79d8f81af3e
Matias Ludueña <matiasluduena23@gmail.com>
2023-05-14 20:36:26 -0300

    add home section

--- src/index.js
+++ src/index.js
@@ -2,4 +2,14 @@ import './styles/main.scss';
 
 import './js/navbar';
 
-console.log("hola")
+import logoImg from './assets/logo.png';
+const logo = document.querySelector('.logo-img');
+logo.src = logoImg;
+
+
+import videoImg from './assets/videoImg.jpg';
+const imgVideo = document.querySelector('.video-img');
+imgVideo.src = videoImg;
+
+
+
