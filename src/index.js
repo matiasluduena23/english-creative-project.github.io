fa072df1f5793271b982433464cd6290afe89abb
Matias Ludueña <matiasluduena23@gmail.com>
2023-05-17 18:39:58 -0300

    add contact page

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import './styles/main.scss';
 
 import './js/navbar';
 
+//insert logo navbar
 import logoImg from './assets/logo.png';
 const logo = document.querySelector('.logo-img');
 logo.src = logoImg;
+//insert log contact
+import logoImgContact from './assets/2.png';
+const contactLogo = document.querySelector('.contact-logo');
+contactLogo.src = logoImgContact;
 
 
 import videoImg from './assets/videoImg.jpg';
--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,17 +1,29 @@
-
+//side navegation toggler
 const hamburger = document.querySelector('.toggle');
-const navbar = document.querySelector('.nav-list');
-
-
+const navlist = document.querySelector('.nav-list');
+const navlink = document.querySelectorAll('.nav-link');
 
 hamburger.addEventListener('click', function(){
-  navbar.classList.toggle('nav-list-active');
+  navlist.classList.toggle('nav-list-active');
   hamburger.classList.toggle('active');
   
 })
 
-// document.querySelectoAll('.nav-link').forEach(n => n.addEventListener('click', () => {
-//   hamburger.classList.remove('active');
-//   navbar.classList.remove('active');
-// }));
+navlink.forEach(n => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navlist.classList.remove('nav-list-active');
+}));
 
+
+//scroll effect navegation
+const navbar = document.querySelector('#navbar');
+const scrollWatcher = document.createElement('div');
+
+scrollWatcher.setAttribute('data-scroll-watcher', '');
+navbar.before(scrollWatcher);
+
+const navObserver = new IntersectionObserver((entries)=> {
+  navbar.classList.toggle('sticking', !entries[0].isIntersecting)
+});
+
+navObserver.observe(scrollWatcher);
